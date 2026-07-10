0c447aa410a355344f4632d584423da3dac6d5c8
elizabethdpenaperez-afk <elizabethdpenaperez@gmail.com>
2026-07-10 19:51:28 -0400

    Update chat welcome message in iniciarChat function

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-function iniciarChat(){
-    alert("🤖 Bienvenido a RoboIA Edu.\n\nMuy pronto estará disponible el asistente inteligente.");
+function iniciarChat() {
+    alert("🤖 ¡Hola! Soy RoboIA Edu.\n\nMuy pronto podrás hacer preguntas sobre:\n\n• Arduino\n• Tinkercad\n• Sensores\n• Programación\n• Inteligencia Artificial");
 }
